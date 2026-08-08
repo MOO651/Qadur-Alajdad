993dcb70713c47ba14f78edf3340e70d6f662810
MOO651 <mo7em@moo.com>
2026-08-08 15:10:30 +0300

    update: تحديث مشروع قدور الأجداد

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,71 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import DailyMenu from './pages/DailyMenu';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Admin from './pages/Admin';
+import EventsMenu from './pages/EventsMenu';
 
 export default function App() {
   const [cart, setCart] = useState<any[]>([]);
+  const [currentPage, setCurrentPage] = useState('daily');
 
   const addToCart = (item: any) => {
     setCart([...cart, item]);
   };
 
-  // وظيفة للتنقل بين الصفحات
+  const removeFromCart = (index: number) => {
+    const updatedCart = [...cart];
+    updatedCart.splice(index, 1);
+    setCart(updatedCart);
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
   const navigateTo = (page: string) => {
-    console.log("Navigating to:", page);
+    setCurrentPage(page);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home navigateTo={navigateTo} addToCart={addToCart} />;
+      case 'daily':
+        return <DailyMenu addToCart={addToCart} />;
+      case 'events':
+        return <EventsMenu weddingMenu={[]} addToCart={addToCart} />;
+      case 'cart':
+        return (
+          <Cart 
+            cartItems={cart} 
+            removeFromCart={removeFromCart} 
+            navigateTo={navigateTo} 
+            clearCart={clearCart} 
+          />
+        );
+      case 'admin':
+        return <Admin navigateTo={navigateTo} />;
+      default:
+        return <DailyMenu addToCart={addToCart} />;
+    }
   };
 
   return (
     <div className="min-h-screen bg-black text-white flex flex-col">
-      {/* تمرير الخصائص المطلوبة للـ Header */}
       <Header 
-        routePath="/" 
+        routePath={currentPage} 
         navigateTo={navigateTo} 
         cartItemsCount={cart.length} 
       />
       
       <main className="flex-grow">
-        <DailyMenu addToCart={addToCart} />
+        {renderPage()}
       </main>
 
-      {/* تمرير الخاصية المطلوبة للـ Footer */}
       <Footer navigateTo={navigateTo} />
     </div>
   );
